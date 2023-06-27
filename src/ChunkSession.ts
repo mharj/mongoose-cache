3f94841d3db3eaf3a5e11222ccce406dc9685968
Marko Harjula <marko.harjula@avanio.com>
2023-06-27 15:11:29 +0300

    chunk session fixes

--- src/ChunkSession.ts
+++ src/ChunkSession.ts
@@ -2,7 +2,14 @@ import {HydratedDocument} from 'mongoose';
 
 export interface AnyCacheSessionChunk {
 	chunk: unknown[];
+	/**
+	 * total amount of data
+	 */
 	total: number;
+	/**
+	 * current amount of data iterated
+	 */
+	current: number;
 }
 
 export interface DocumentCacheSessionChunk<T, DocType extends HydratedDocument<T>> extends AnyCacheSessionChunk {
@@ -14,9 +21,11 @@ export class ChunkSession<T, DocType extends HydratedDocument<T> = HydratedDocum
 	constructor(data: DocType[], size: number) {
 		const chunks: DocumentCacheSessionChunk<T, DocType>[] = [];
 		for (let i = 0; i < data.length; i += size) {
+			const chunk = data.slice(i, i + size);
 			chunks.push({
-				chunk: data.slice(i, i + size),
+				chunk,
 				total: data.length,
+				current: i + chunk.length,
 			});
 		}
 		this.iteratorData = new Set(chunks);
